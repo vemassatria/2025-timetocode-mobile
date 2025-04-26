fd55fda450db723cf61451602bdce83e1f36d492
Ivanrhmt77 <ivanrahmat.p77@gmail.com>
2025-04-26 23:21:13 +0700

    Menambahkan fungsi menutup overlay dan kembali ke overlay sebelumnya

--- lib/utils/overlay_utils.dart
+++ lib/utils/overlay_utils.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 
+OverlayEntry? _currentOverlay;
+Widget? _previousContent;
+
 void showPopupOverlay(BuildContext context, Widget popupContent) {
   final overlay = Overlay.of(context);
 
-  final overlayEntry = OverlayEntry(
-    builder: (context) {
-      return Material(
-        color: Color(0x80000000),
-        child: Center(child: popupContent),
-      );
-    },
+  if (_currentOverlay != null) {
+    _previousContent = _currentOverlay!.builder(context);
+    _currentOverlay!.remove();
+  }
+
+  _currentOverlay = OverlayEntry(
+    builder:
+        (context) => Material(
+          color: Color(0x80000000),
+          child: Center(child: popupContent),
+        ),
   );
 
-  overlay.insert(overlayEntry);
+  overlay.insert(_currentOverlay!);
+}
+
+void closePopupOverlay() {
+  _currentOverlay?.remove();
+  _currentOverlay = null;
+  _previousContent = null;
+}
+
+void goBackToPreviousOverlay(BuildContext context) {
+  if (_previousContent != null) {
+    final previousWidget = _previousContent;
+    _currentOverlay?.remove();
+    _currentOverlay = OverlayEntry(builder: (context) => previousWidget!);
+    Overlay.of(context).insert(_currentOverlay!);
+    _previousContent = null;
+  } else {
+    closePopupOverlay();
+  }
 }
