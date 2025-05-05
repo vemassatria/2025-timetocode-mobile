ba5d65442448826de2557c596142dde5313a39e0
Moch. Alif Akbar <149356638+mochalifakbar@users.noreply.github.com>
2025-05-05 14:18:10 +0700

    membuat keystone

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -20,22 +19,35 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.drittelegiona.timetocode"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk        = flutter.minSdkVersion
+        targetSdk     = flutter.targetSdkVersion
+        versionCode   = flutter.versionCode
+        versionName   = flutter.versionName
+    }
+
+    // 1) Definisikan signingConfigs
+    signingConfigs {
+        create("release") {
+            storeFile     = file("drittelegiona.keystore")
+            storePassword = "A3Solid"
+            keyAlias      = "timetocode-keyapp"
+            keyPassword   = "A3Solid"
+        }
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        // 2) Konfigurasi buildTypes.release
+        getByName("release") {
+            signingConfig   = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
+        // (opsional) debug tetap default
     }
 }
 
