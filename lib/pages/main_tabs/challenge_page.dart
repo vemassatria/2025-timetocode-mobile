5dceed4d02dad1958dc286bc013666b0f3e4ab17
Moch. Alif Akbar <149356638+mochalifakbar@users.noreply.github.com>
2025-05-29 16:55:33 +0700

    mengumpulkan challenge

--- lib/pages/main_tabs/challenge_page.dart
+++ lib/pages/main_tabs/challenge_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:timetocode/components/challengecard.dart';
 import 'package:timetocode/games/backend/providers/challenge_provider.dart';
 import 'package:timetocode/games/backend/providers/daftar_challenge_provider.dart';
-import 'package:timetocode/pages/main_tabs/quiz_page.dart';
 import 'package:timetocode/themes/colors.dart';
 import 'package:timetocode/themes/typography.dart';
 //import 'package:timetocode/games/backend/controllers/challenge_controller.dart';
@@ -16,70 +15,64 @@ class ChallengePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final levelCompleted = ref.watch(completedChallengeProvider);
     final notifier = ref.read(completedChallengeProvider.notifier);
-    final storyState = ref.watch(challengeControllerProvider).value!;
+    final challengeAsync = ref.watch(challengeControllerProvider);
 
-    return Scaffold(
-      backgroundColor: AppColors.darkBackground,
-      appBar: AppBar(
-        backgroundColor: AppColors.surfaceDark,
-        elevation: 0,
-        title: Text(
-          storyState.challenge.title,
-          style: AppTypography.heading6(color: AppColors.white),
-        ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 16.w,
-            mainAxisSpacing: 24.w,
-            childAspectRatio: 1,
+    return challengeAsync.when(
+      loading:
+          () => const Scaffold(
+            backgroundColor: AppColors.darkBackground,
+            body: Center(child: CircularProgressIndicator()),
           ),
-          itemCount: storyState.challenge.levels.length,
-          itemBuilder: (context, index) {
-            final level = storyState.challenge.levels[index];
-            final levelNumber = level.id;
-            final isUnlocked = levelCompleted <= levelNumber + 1;
+      error:
+          (error, stack) => Scaffold(
+            backgroundColor: AppColors.darkBackground,
+            body: Center(child: Text('Error loading levels')),
+          ),
+      data: (challengeState) {
+        return Scaffold(
+          backgroundColor: AppColors.darkBackground,
+          appBar: AppBar(
+            backgroundColor: AppColors.surfaceDark,
+            elevation: 0,
+            title: Text(
+              challengeState.challenge.title,
+              style: AppTypography.heading6(color: AppColors.white),
+            ),
+            centerTitle: true,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16.w,
+                mainAxisSpacing: 24.w,
+                childAspectRatio: 1,
+              ),
+              itemCount: challengeState.challenge.levels.length,
+              itemBuilder: (context, index) {
+                final level = challengeState.challenge.levels[index];
+                final levelNumber = level.id;
+                final isUnlocked = (levelCompleted + 1) >= levelNumber;
 
-            return ChallengeCard(
-              levelNumber: levelNumber,
-              starCount: notifier.getCompletedChallengeStars(levelNumber),
-              isUnlocked: isUnlocked,
-              onTap:
-                  isUnlocked
-                      ? () {
-                        // Navigasi ke halaman challenge detail ketika card diklik
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder:
-                                (context) => QuizPage(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  correctAnswer: 1,
-                                  wrongAnswer: 2,
-                                  question:
-                                      "Yang manakah output yang benar dari kode di bawah ini?",
-                                  code:
-                                      "#include <stdio.h>\n\nint main() {\n  int age = 25;\n\n  printf(\"Age: %d\\n\", age);\n\n  return 0;\n}",
-                                  options: [
-                                    "Age: %d\\n",
-                                    "Age: 25",
-                                    "Age: %d",
-                                    "Age: 25\\n",
-                                  ],
-                                ),
-                          ),
-                        );
-                      }
-                      : null,
-            );
-          },
-        ),
-      ),
+                return ChallengeCard(
+                  levelNumber: levelNumber,
+                  starCount: notifier.getCompletedChallengeStars(levelNumber),
+                  isUnlocked: isUnlocked,
+                  onTap:
+                      isUnlocked
+                          ? () {
+                            ref
+                                .read(challengeControllerProvider.notifier)
+                                .startChallenge(index);
+                          }
+                          : null,
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 
