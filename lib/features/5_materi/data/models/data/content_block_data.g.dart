b387a72c995a565e73cdce0815cd365015d1ebd4
Moch. Alif Akbar <149356638+mochalifakbar@users.noreply.github.com>
2025-10-24 22:28:36 +0700

    feat(story): add dialog variant and stat for level 1-5 (Ref #103)

--- lib/features/5_materi/data/models/data/content_block_data.g.dart
+++ lib/features/5_materi/data/models/data/content_block_data.g.dart
@@ -49,10 +49,8 @@ Map<String, dynamic> _$ImageDataToJson(ImageData instance) => <String, dynamic>{
 };
 
 TableData _$TableDataFromJson(Map<String, dynamic> json) => TableData(
-  headers: (_readHeaders(json, 'headers') as List<dynamic>)
-      .map((e) => e as String)
-      .toList(),
-  rows: (_readRows(json, 'rows') as List<dynamic>)
+  headers: (json['headers'] as List<dynamic>).map((e) => e as String).toList(),
+  rows: (json['rows'] as List<dynamic>)
       .map((e) => (e as List<dynamic>).map((e) => e as String).toList())
       .toList(),
   $type: json['type'] as String?,
